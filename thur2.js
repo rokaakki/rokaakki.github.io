d38d23a042b9ce846074cb170e4f248ef79b5910
rokaakki <ni.mu.0901@gmail.com>
2015-10-15 22:53:23 -0500

    quest url fix

--- thur2.js
+++ thur2.js
@@ -12,7 +12,7 @@
 		next:function(){
 			if(!start)
 				return;
-			if(/quest\//i.test(location.hash)){
+			if(/quest/i.test(location.hash)){
 				if($(".btn-favorite.visible").length){
 					//click favorite
 					$(".btn-favorite.visible").trigger("tap");
@@ -65,7 +65,7 @@
 	,cc = $('<div class="wg"><style>.wg{position:absolute;z-index:250001;top:2px;left:2px}.wg button{width:42px;height:22px;margin-right:4px}</style></div>').appendTo(document.body)
 	,cmd = $('<button style="width:52px">stop</button>').appendTo(cc);
 	cmd.on(et,function(){
-		if(!(/quest\//i.test(location.hash)||/raid\//i.test(location.hash)))
+		if(!(/quest/i.test(location.hash)||/raid\//i.test(location.hash)))
 			return alert("Page error!");
 		start=!start;
 		if(start==true){
