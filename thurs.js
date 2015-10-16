8ff0f5f0f4d73d38f10eac3b82a161dd3633c772
rokaakki <ni.mu.0901@gmail.com>
2015-10-15 22:12:06 -0500

    Revert "Revert "change to favorite""
    This reverts commit 3ec555969d521821f984389052a367e531565ed9.

--- thurs.js
+++ thurs.js
@@ -1,47 +1,64 @@
 (function(){
 	var start = true;
-	var battle3 = false;
+	var battle = localStorage.getItem("battle")?true:false;
+	var questNo = 1;
+	var episode = 3;
+
 	var fn = {
 		sleep:function(next){
 				var slt=1.5+Math.random();
-				if(start)
-					setTimeout(next,slt*1000);
+				setTimeout(next,slt*1000);
 		},
 		next:function(){
-			if($(".prt-use-button").find(".btn-use-full").length){
-				$(".prt-use-button").find(".btn-use-full").trigger("tap");
-			}else if($("div[data-quest-id='400091']").length){
-				$("div[data-quest-id='400091']").trigger("tap");
-			}else if($(".lis-supporter").eq(5).length){
-				$(".lis-supporter").eq(5).trigger("tap");
-			}else if($(".se-quest-start").length){
-				$(".se-quest-start").trigger("tap");
-				battle3=false;
-			}else if($(".prt-battle-num").find(".num-info3").length>1){
-				$(".prt-ability-list").find(".ability-character-num-1-4").trigger("tap");
-
+			if(!start)
+				return;
+			if(/quest/i.test(location.hash)){
+				if($(".btn-favorite.visible").length){
+					$(".btn-favorite.visible").trigger("tap");
+				}else if($(".prt-popup-body").find(".btn-cleard-episode").eq(episode).length){
+					$(".prt-popup-body").find(".btn-cleard-episode").eq(episode).trigger("tap");
+				}else if($(".prt-use-button").find(".btn-use-full").length){
+					$(".prt-use-button").find(".btn-use-full").trigger("tap");
+				}else if($(".prt-noindex-list").find(".prt-list-contents").eq(questNo).length){
+					$(".prt-noindex-list").find(".prt-list-contents").eq(questNo).trigger("tap");
+				}else if($(".lis-supporter").eq(5).length){
+					$(".lis-supporter").eq(5).trigger("tap");
+				}else if($(".cnt-quest-command").find(".btn-command-forward:visible").length){
+					$(".cnt-quest-command").find(".btn-command-forward:visible").trigger("tap");
+				}else if($(".se-quest-start:visible").length){
+					battle=true;
+					localStorage.setItem("battle",true);
+					$(".se-quest-start").trigger("tap");
+				}
+				fn.sleep(fn.next);
+			}else if(/raid\//i.test(location.hash))&&battle){
+				if($(".prt-battle-num").find(".num-info3").length>1){
+					$(".prt-ability-list").find(".ability-character-num-1-4").trigger("tap");
+				}
 				if($('.btn-attack-start.display-on').length){
 					$('.btn-attack-start.display-on').trigger('tap');
-				}else{
+				}else if($('.btn-result:visible').length){
 					$('.btn-result:visible').trigger('tap');
+				}else if($(".pop-usual.pop-exp.pop-show").find(".btn-usual-ok").length){
+					$(".pop-usual.pop-exp.pop-show").find(".btn-usual-ok").trigger("tap");
+				}else if($(".prt-button-area").find(".btn-control").length){
+					battle=false;
+					localStorage.setItem("battle",false);
+					$(".prt-button-area").find(".btn-control").trigger("tap");
 				}
-			}else if($('.btn-attack-start.display-on').length){
-				$('.btn-attack-start.display-on').trigger('tap');
-			}else if($('.btn-result:visible').length){
-				$('.btn-result:visible').trigger('tap');
-			}else if($(".pop-usual.pop-exp.pop-show").find(".btn-usual-ok").length){
-				$(".pop-usual.pop-exp.pop-show").find(".btn-usual-ok").trigger("tap");
-			}else if($(".prt-button-area").find(".btn-control").length){
-				$(".prt-button-area").find(".btn-control").trigger("tap");
-			}else if($(".pop-friend-request").find(".btn-usual-cancel").length){
-				$(".pop-friend-request").find(".btn-usual-cancel").trigger("tap");
-			}
-			fn.sleep(fn.next);
+				fn.sleep(fn.next);
+			}else{
+				start=false;
+				cmd.text("start");
+			}	
 		}
 	},et = 'ontouchstart' in window ? 'touchstart' : 'mousedown'
 	,cc = $('<div class="wg"><style>.wg{position:absolute;z-index:250001;top:2px;left:2px}.wg button{width:42px;height:22px;margin-right:4px}</style></div>').appendTo(document.body)
-	,cmd = $('<button style="width:52px">stop</button>').appendTo(cc);
+	,cmd = $('<button style="width:52px">start</button>').appendTo(cc);
 	cmd.on(et,function(){
+		if(!(/quest\/extra/i.test(location.hash)||/raid\//i.test(location.hash)))
+			alert("Page error!");
+			return;
 		start=!start;
 		if(start==true){
 			fn.next();
