1940ac566c56b6498852603d1a4bf760c9d445f2
rokaakki <ni.mu.0901@gmail.com>
2015-10-16 23:05:52 -0500

    hlaf/full

--- thur2.js
+++ thur2.js
@@ -1,5 +1,6 @@
 (function(){
 	var start = true;
+	var useHalf = true;
 	var battle = localStorage.getItem("battle")=="true"?true:false;
 	var questNo = localStorage.getItem("questNo")?parseInt(localStorage.getItem("questNo")):0;
 	var episode = localStorage.getItem("episode")?parseInt(localStorage.getItem("episode")):2;
@@ -12,6 +13,7 @@
 		next:function(){
 			if(!start)
 				return;
+			var useBtn = useHalf?'.btn-use-other':'.btn-use-full';
 			if(/quest/i.test(location.hash)){
 				if($(".btn-favorite.visible").length){
 					//click favorite
@@ -19,9 +21,9 @@
 				}else if($(".prt-popup-body").find(".btn-cleard-episode").eq(episode).length){
 					//click episode (higher than quest)
 					$(".prt-popup-body").find(".btn-cleard-episode").eq(episode).trigger("tap");
-				}else if($(".prt-use-button").find(".btn-use-full").length){
+				}else if($(".prt-use-button").find(useBtn).length){
 					//click full ap (higher than quest)
-					$(".prt-use-button").find(".btn-use-full").trigger("tap");
+					$(".prt-use-button").find(useBtn).trigger("tap");
 				}else if($(".se-quest-start:visible").length){
 					//start quest (higher than ok)
 					battle=true;
@@ -67,21 +69,22 @@
 	},et = 'ontouchstart' in window ? 'touchstart' : 'mousedown'
 	,cc = $('<div class="wg"><style>.wg{position:absolute;z-index:250001;top:2px;left:2px}.wg button{width:42px;height:22px;margin-right:4px}</style></div>').appendTo(document.body)
 	,cmd = $('<button style="width:52px">stop</button>').appendTo(cc)
+	,cmd2 = $('<button style="width:52px">half</button>').appendTo(cc)
 	,input1 = $('<input style="width:32px" />').appendTo(cc)
 	,input2 = $('<input style="width:32px" />').appendTo(cc);
-	input1.val(questNo);
-	input2.val(episode);
+	input1.val((questNo+1));
+	input2.val((episode+1));
 	input1.change(function(){
 		var newVal = parseInt(input1.val());
-		if(newVal>-1&&newVal<5){
-			questNo = newVal;
+		if(newVal>0&&newVal<6){
+			questNo = newVal-1;
 			localStorage.setItem("questNo",questNo);
 		}
 	});
 	input2.change(function(){
 		var newVal = parseInt(input2.val());
-		if(newVal>-1&&newVal<4){
-			episode = newVal;
+		if(newVal>0&&newVal<5){
+			episode = newVal-1;
 			localStorage.setItem("episode",episode);
 		}
 	});
@@ -89,11 +92,19 @@
 		if(!(/quest/i.test(location.hash)||/raid\//i.test(location.hash)||/result\//i.test(location.hash)))
 			return alert("Page error!");
 		start=!start;
-		if(start==true){
+		if(start){
 			fn.next();
 			cmd.text("stop");
 		}else{
 			cmd.text("start");
+		}
+	});
+	cmd2.on(et,function(){
+		useHalf=!useHalf;
+		if(useHalf){
+			cmd2.text("half");
+		}else{
+			cmd2.text("full");
 		}
 	});
 	fn.sleep(fn.next);
