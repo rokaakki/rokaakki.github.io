160316c4a7f770fb9c8acbd79cb887666c1b52d7
rokaakki <ni.mu.0901@gmail.com>
2015-10-25 23:30:12 -0500

    ex auto fix

--- battle.js
+++ battle.js
@@ -8,7 +8,20 @@ var fn = {
 			return;
 		if(/raid\//i.test(location.hash)&&battle){
 			if($(".prt-battle-num").find(".num-info3").length>1){
-				$(".prt-ability-list").find(".ability-character-num-1-4").trigger("tap");
+				//$(".prt-ability-list").find(".ability-character-num-1-4").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-1").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-2").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-3").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-4").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-2-1").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-2-2").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-2-3").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-3-1").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-3-2").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-3-3").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-4-1").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-4-2").trigger("tap");
+				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-4-3").trigger("tap");
 			}
 			if($('.btn-attack-start.display-on').length){
 				$('.btn-attack-start.display-on').trigger('tap');
