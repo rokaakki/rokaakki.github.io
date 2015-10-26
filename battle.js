88c12d7a1f7db29f3fe48dc69096172ebf55f87c
rokaakki <ni.mu.0901@gmail.com>
2015-10-25 23:51:22 -0500

    skill fix

--- battle.js
+++ battle.js
@@ -7,26 +7,20 @@ var fn = {
 		if(!start)
 			return;
 		if(/raid\//i.test(location.hash)&&battle){
+			var skill = false;
 			if($(".prt-battle-num").find(".num-info3").length>1){
 				//$(".prt-ability-list").find(".ability-character-num-1-4").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-1").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-2").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-3").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-1-4").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-2-1").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-2-2").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-2-3").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-3-1").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-3-2").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-3-3").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-4-1").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-4-2").trigger("tap");
-				$(".prt-ability-list").find(".btn-ability-available").find(".ability-character-num-4-3").trigger("tap");
+				if($(".prt-ability-list").find(".btn-ability-available").length){
+					skill=true;
+					$(".prt-ability-list").find(".btn-ability-available").eq(0).trigger("tap");
+				}
 			}
-			if($('.btn-attack-start.display-on').length){
-				$('.btn-attack-start.display-on').trigger('tap');
-			}else if($('.btn-result:visible').length){
-				$('.btn-result:visible').trigger('tap');
+			if(!skill){
+				if($('.btn-attack-start.display-on').length){
+					$('.btn-attack-start.display-on').trigger('tap');
+				}else if($('.btn-result:visible').length){
+					$('.btn-result:visible').trigger('tap');
+				}
 			}
 			
 		}
