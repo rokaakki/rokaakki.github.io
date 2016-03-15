c3e1de003ffb2fce98e38f4e8092e227a8e49db7
rokaakki <ni.mu.0901@gmail.com>
2016-03-15 11:31:48 -0500

    Update battle.js

--- battle.js
+++ battle.js
@@ -12,20 +12,20 @@ var fn = {
 			}else{
 				var skill = false;
 				var ability = $(".prt-command-chara:not(.ability-disable)").find(".prt-ability-list").find(".btn-ability-available");
-				// if($(".prt-battle-num").find(".num-info3").length>1){
-				// 	if(!skill&&ability.find(".ability-character-num-1-4").length){
-				// 		ability.find(".ability-character-num-1-4").trigger("tap");
-				// 		skill = true;
-				// 	}else if(!skill&&ability.length){
-				// 		if(ability.eq(0).find('div[ability-id="711"]').length||
-				// 			ability.eq(0).find('div[ability-id="3014"]').length){
+				if($(".prt-battle-num").find(".num-info3").length>1){
+					if(!skill&&ability.find(".ability-character-num-1-4").length){
+						ability.find(".ability-character-num-1-4").trigger("tap");
+						skill = true;
+					}else if(!skill&&ability.length){
+						if(ability.eq(0).find('div[ability-id="711"]').length||
+							ability.eq(0).find('div[ability-id="3014"]').length){
 							
-				// 		}else{
-				// 			skill = true;
-				// 			ability.eq(0).trigger("tap");
-				// 		}
-				// 	}
-				// }
+						}else{
+							skill = true;
+							ability.eq(0).trigger("tap");
+						}
+					}
+				}
 				if(!skill){
 					if($('.btn-attack-start.display-on').length){
 						$('.btn-attack-start.display-on').trigger('tap');
